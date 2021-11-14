a9b457513d084b3fd4b7b9b8a0c9069bd5611df8
Farhd Islam <farhad.islam@outlook.com>
2021-11-14 23:06:49 +0000

    added a date validation

---
+++ date.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+echo "Date DEMO"
+
+read -p "Enter a date: " date
+
+pattern="^[0-9]{8}$"
+
+if [[ $date =~$pattern ]]; then 
+   echo "$date is valid"
+else
+   echo "$date is not valid"
+fi
+
+

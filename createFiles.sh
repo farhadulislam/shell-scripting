197489037260ff6f6be80b3af028396cafa203bc
Farhd Islam <farhad.islam@outlook.com>
2022-05-30 11:44:39 +0600

    Updated createFiles.sh - removed for loop for file creation but instead added one for putting data into files

--- createFiles.sh
+++ createFiles.sh
@@ -2,6 +2,8 @@
 # Author: Md Farhad Ul Islam
 # Version : 1.0.0
 
+
+echo " "
 echo "PROVIDE TWO ARGS, FIRST for file ext such as txt, pdf, csv and SECOND for number of files you wish to create"
 echo " "
 sleep 2
@@ -18,15 +20,19 @@ DIR=${3:-TEST_FILES}
 
 cd $DIR || echo "cd into $DIR failed"
 
-for i in $NUM_OF_FILES
-  do 
-  time touch $(seq -f "dummy-%02g.$FILE_TYPE" 1 $NUM_OF_FILES)
-  ls *.$FILE_TYPE
-  echo "NUMER of files" && (ls *.$FILE_TYPE | wc -l)
-  done
+time touch $(seq -f "dummy-%02g.$FILE_TYPE" 1 $NUM_OF_FILES)
+ls *.$FILE_TYPE
+echo "FILES CREATED: $(ls *.$FILE_TYPE | wc -l) " 
+
 
 echo " "
-echo " #You can run the following if you wish to rename files"
+for i in $(ls *.$FILE_TYPE)
+ do 
+  echo "Some test data" > $i
+ done
+
+echo " "
+echo "#You can run the following if you wish to rename files"
 echo "ls | cut -d. -f1 | xargs -I {}  mv {}.txt {}.text"
 echo "***************END*******************************"
 

cef60a3c078ecdfc73072da1ee1bd2cfeff5f6e2
Farhd Islam <farhad.islam@outlook.com>
2022-06-01 16:40:31 +0600

    Added a for loop demo

---
+++ demo_forLoop.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+# Author: Md Farhad Ul Islam
+# Created : 1st June 2022.
+
+
+for row in 1 2 3 4 5 
+    do
+        for col in 1 2 3 4 5 
+        do
+            
+        done 
+        echo $row $col
+    done

b6efd84e39df76bfa805f9c61fcf2bb1aedf83e7
Farhd Islam <farhad.islam@outlook.com>
2022-07-10 20:46:17 +0600

    Updated demo while until

--- scripts/demo_while_until_loop.sh
+++ scripts/demo_while_until_loop.sh
@@ -2,13 +2,20 @@
 # Author : Farhad
 # Date created : 10 July 2022
 
-: 'multi line comments'
+: 'multi 
+line 
+comments'
+function while_demo1(){
 COUNTER=0;
 while [ $COUNTER -lt 10 ]; do
     echo The counter is $COUNTER
     printf "\n"
     let COUNTER+=1
 done
+}
+
+
+function while_demo2(){
 
 printf "\nUNTIL DEMO\n"
 echo 
@@ -17,5 +24,26 @@ until [ $COUNTER2 -lt 10 ]; do
     echo The counter2 is $COUNTER2
     let COUNTER2-=1
 done
+}
+
+function while_demo3(){
+COUNTER3=0;
+while [ $COUNTER3 -lt 10 ]; do
+    echo CREATING FILES ...
+    touch file$COUNTER3.txt
+    printf "\n"
+    let COUNTER3+=1
+done 
+}
+
+#calling function while_demo1
+if (while_demo1)
+then 
+    echo "while_demo1 ran"
+else 
+    echo "Failed"
+fi
+
+
 
 

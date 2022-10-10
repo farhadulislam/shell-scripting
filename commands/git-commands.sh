2d23c90846d8bc8512a520d3642bc2030ff65c4e
Md Farhad Islam <farhad.islam@outlook.com>
2022-10-10 14:38:14 +0100

    added git commands file

---
+++ commands/git-commands.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+# Author : Farhad Islam
+#Version : 1.0.0
+#Useful git commands.
+
+git reflog
+git remote add origin https://github.com/farhadulislam/modern-java-exercises.git
+git push --set-upstream origin master
+git gc --prune=all
+

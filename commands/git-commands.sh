e88bac3393fba4ad3bf8c4c2745f701a33a698aa
Farhd Islam <farhad.islam@outlook.com>
2023-04-18 23:47:12 +0100

    Added git cmds

--- commands/git-commands.sh
+++ commands/git-commands.sh
@@ -3,8 +3,43 @@
 #Version : 1.0.0
 #Useful git commands.
 
-git reflog
+
 git remote add origin https://github.com/farhadulislam/modern-java-exercises.git
 git push --set-upstream origin master
 git gc --prune=all
 
+#git log
+git log --stat
+#This command shows the commit's history including all files and their changes
+git log -p
+git reflog
+
+#git add 
+git add -p
+
+#git diff
+git diff
+git diff main.java
+git diff --staged
+
+#Git bisect 
+git bisect good
+git bisect bad
+git bisect start
+git bisect reset
+
+#store login credentials in the cache so you don't have to type them in each time
+git config --global credential.helper cache
+
+#With the asterisk in the command below, you can add all files starting with 'fil' in the staging area
+git add fil*
+
+#Git remote
+git remote -v
+
+#git branch
+
+git branch -r
+git branch -a
+git branch -vva
+git branch -vv
